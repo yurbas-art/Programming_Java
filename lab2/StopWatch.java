41415f3e4c32ad16c74af6a9454e00c4bbe09dfa
wh1sper <70734690+yurbas-art@users.noreply.github.com>
2026-04-01 20:21:18 +0800

    Create StopWatch.java

---
+++ lab2/StopWatch.java
@@ -0,0 +1,47 @@
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100000);
+        }
+        StopWatch sw = new StopWatch();
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        sw.stop();
+        System.out.println(sw.getElapsedTime());
+    }
+}
